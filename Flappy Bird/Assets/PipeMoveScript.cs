455f16b6e0934e50d6963fe2c7573946f96b1635
edielam <edguard738@gmail.com>
2023-02-01 10:35:05 +0000

    scoring

--- Flappy Bird/Assets/PipeMoveScript.cs
+++ Flappy Bird/Assets/PipeMoveScript.cs
@@ -19,6 +19,7 @@ public class PipeMoveScript : MonoBehaviour
         
         if(transform.position.x < deadZone)
         {
+            Debug.Log("Pipe deleted");
             Destroy(gameObject);
         }
     }
