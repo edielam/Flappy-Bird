4058a695f32be94f66f789c2577e1c48b9ba1612
edielam <edguard738@gmail.com>
2023-02-01 17:38:10 +0000

    game over and restart

--- Flappy Bird/Assets/LogicScript.cs
+++ Flappy Bird/Assets/LogicScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
@@ -13,5 +14,10 @@ public class LogicScript : MonoBehaviour
     {
         playerScore = playerScore + addThis;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
