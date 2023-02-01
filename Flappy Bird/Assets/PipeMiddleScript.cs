c19892cf66240429d0bfebf4e64fb11322793ed4
edielam <edguard738@gmail.com>
2023-02-01 15:59:29 +0000

    colliders

---
+++ Flappy Bird/Assets/PipeMiddleScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMiddleScript : MonoBehaviour
+{
+    public LogicScript logic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        logic.addScore();        
+    }
+}
